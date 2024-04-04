a02bf0ed4a8c5e639ebb46437fc87a0d34125458
Harsh chitaliya <124803206+harshchi19@users.noreply.github.com>
2024-04-04 22:40:59 +0530

    Update hotel_management_analysis.py

--- hotel_management_analysis.py
+++ hotel_management_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
@@ -110,3 +111,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
