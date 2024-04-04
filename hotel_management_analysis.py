33c693e53058187767570d69dea363da5044da7b
Harsh chitaliya <124803206+harshchi19@users.noreply.github.com>
2024-04-04 22:33:24 +0530

    Update hotel_management_analysis.py

--- hotel_management_analysis.py
+++ hotel_management_analysis.py
@@ -1,6 +1,10 @@
+import matplotlib
+matplotlib.use('agg')  # Specify the 'agg' backend
+
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
